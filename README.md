a041e0c56593d366e0a603e6a61929adda9f7b60
jiangkairuiRain <jjj857857857@163.com>
2025-08-27 17:59:27 +0800

    修改README.md 和 start_moon_widget.bat

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # moon_widget
-a small tool on desktop to show information about the moon
+
+### a widget on desktop to show information about the moon
+
+<br>
+
+- 1.PS:请提前安装好python和以下库（记得把python加入path）:
+
+        pip install webview pytz requests pywin32 geoip2 -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+<br>
+
+- 2.Win+R，对话框中输入shell:startup打开文件夹，将start_moon_widget.bat的快捷方式放入此文件夹（实现开机自启功能）
+
+<br>
+
+- 3.在桌面上右键，选择"新建" -> "快捷方式"。在位置字段中输入：
+
+	    pythonw.exe "完整路径\moon_widget\moon_widget.py"
+
+    （将"完整路径"替换为真实文件路径）（实现创建快捷方式功能）
+
+<br>
+
+- 4.把2、3中创建的快捷方式，"右键"->"属性"->"快捷方式"->"更改图标"->"浏览"，选择文件夹中的moon.ico（更改图标）
--- start_moon_widget.bat
+++ start_moon_widget.bat
@@ -2,19 +2,6 @@
 chcp 65001 > nul
 title Moon Widget Launcher
 
-REM 检查是否已经隐藏了窗口
-if "%1"=="hidden" goto main
-
-REM 第一次运行，使用VBScript隐藏窗口并重新启动
-echo Set UAC = CreateObject("Shell.Application") > "%temp%\hidden.vbs"
-echo UAC.ShellExecute "%~f0", "hidden", "", "runas", 0 >> "%temp%\hidden.vbs"
-"%temp%\hidden.vbs"
-exit /b
-
-:main
-REM 删除临时VBScript文件
-if exist "%temp%\hidden.vbs" del "%temp%\hidden.vbs"
-
 REM 切换到脚本所在目录
 cd /d "%~dp0"
 
@@ -22,10 +9,10 @@ REM 检查pythonw是否可用
 where pythonw >nul 2>nul
 if %errorlevel% equ 0 (
     echo 使用pythonw启动月球位置小部件...
-    pythonw moon_widget.py
+    start "" pythonw moon_widget.py
 ) else (
     echo pythonw不可用，尝试使用python...
-    python moon_widget.py
+    start "" python moon_widget.py
 )
 
 REM 如果以上都失败，显示错误信息
